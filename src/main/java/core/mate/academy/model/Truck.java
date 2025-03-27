a535764dbb0beb8ae9a49506e6674dc4b1b6e77b
DZotsenko <diana.zotsenko@gmail.com>
2025-03-27 14:10:25 +0200

    added solution

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,6 +5,10 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private double cargoCapacity;
+    private String cargoType;
+    private boolean hasTrailer;
+
     public Truck() {
     }
 
---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,19 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    private static final int EXCAVATOR_NUMBER = 3;
+
+    @Override
+    public List<Excavator> get() {
+        List<Excavator> excavators = new ArrayList<>();
+        for (int i = 0; i < EXCAVATOR_NUMBER; i++) {
+            excavators.add(new Excavator());
+        }
+        return excavators;
+    }
+}
